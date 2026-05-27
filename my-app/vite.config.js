4e5ed878f1550adf4eb43607bb8946d115c643fc
김수현(hvl1789) <hvl1789@gepplus.net>
2026-05-27 15:15:24 +0900

    vercel!

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      react: fileURLToPath(new URL('./node_modules/react', import.meta.url)),
+      'react': resolve(__dirname, 'node_modules/react'),
+      'react-dom': resolve(__dirname, 'node_modules/react-dom'),
     },
   },
 })
